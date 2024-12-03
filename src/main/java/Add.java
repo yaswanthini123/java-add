a433916f90458ba2c36dd3aef5e69fec184c6ac6
Buddha Yaswanthini <buddhayaswanthini123@outlook.com>
2024-12-03 14:14:45 +0530

    committing all the changes

---
+++ src/main/java/Add.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Add
+{
+public static void main(String args[])
+{
+int a,b;
+System.out.println("enter the value of a:");
+Scanner s=new Scanner(System.in);
+a = s.nextInt();
+System.out.println("enter the value of b:");
+b = s.nextInt();
+System.out.println("sum is :"+(a+b));
+}
+}
